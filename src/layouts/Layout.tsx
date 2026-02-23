c9d032b2562d3e4a8e69c2b7690a98a58772b036
Daniel Montiel <danielponce1397@gmail.com>
2026-02-23 11:01:03 -0600

    Agregar boton para documentación api

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -6,10 +6,24 @@ export default function Layout() {
     return (
         <>
             <header className="bg-slate-700">
-                <div className="mx-auto max-w-5xl px-4 sm:px-6 lg:px-8 py-6 sm:py-10">
+                <div className="mx-auto max-w-5xl px-4 sm:px-6 lg:px-8 py-6 sm:py-10 
+                        flex flex-col sm:flex-row 
+                        sm:items-center sm:justify-between 
+                        gap-4 sm:gap-0">
+
                     <h1 className="text-2xl sm:text-3xl lg:text-4xl font-extrabold text-white">
                         Administrador de Productos
                     </h1>
+
+                    <button
+                        onClick={() => window.open(`${import.meta.env.VITE_BACKEND_URL}/docs`, '_blank')}
+                        className="bg-white text-slate-700 font-semibold px-4 py-2 
+                           rounded-lg shadow hover:bg-slate-100 transition
+                           w-full sm:w-auto"
+                    >
+                        Documentación API
+                    </button>
+
                 </div>
             </header>
 
